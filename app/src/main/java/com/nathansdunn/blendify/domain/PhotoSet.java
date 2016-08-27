350223bb8f1d678f98d4e01f493638a1a35656d7
Nathan Dunn <nathan.dunn@UNSWalumni.com>
2016-08-26 21:36:09 -0700

    Correct %d to %s

--- app/src/main/java/com/nathansdunn/blendify/domain/PhotoSet.java
+++ app/src/main/java/com/nathansdunn/blendify/domain/PhotoSet.java
@@ -1,10 +1,13 @@
 package com.nathansdunn.blendify.domain;
 
+import android.graphics.Bitmap;
 import android.os.Environment;
 import android.util.Log;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 
 /**
  * Created by nathan on 25/08/16.
@@ -14,7 +17,6 @@ public class PhotoSet {
     private static final String subDir = "/DCIM/youtruvian/";
 
     private String id;
-
     private File folder;
 
     public PhotoSet(String id) {
@@ -23,7 +25,6 @@ public class PhotoSet {
 
     private File getStorage() {
         String sdcard = System.getenv("SECONDARY_STORAGE");
-
 
         if ((sdcard == null) || (sdcard.length() == 0)) {
             sdcard = System.getenv("EXTERNAL_SDCARD_STORAGE");
@@ -44,7 +45,7 @@ public class PhotoSet {
                 Log.d(TAG, "created directory:"+folder.getAbsoluteFile());
             }
         } else {
-            throw new UnsupportedOperationException(String.format("Directory %d already exists", folder.getAbsoluteFile()));
+            throw new UnsupportedOperationException(String.format("Directory %s already exists", folder.getAbsoluteFile()));
         }
     }
 
@@ -53,5 +54,17 @@ public class PhotoSet {
         File file = new File(folder +"/photo_"+id+".jpg");
         if (!file.exists()) file.createNewFile();
         return file;
+    }
+
+    public void savePhoto(int id, Bitmap bitmap) throws IOException {
+        String loc = getPhoto(id).getAbsolutePath();
+        if (loc != null) {
+            try {
+                OutputStream os = new FileOutputStream(loc);
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, os);
+            } catch(IOException e) {
+                throw new IOException("Problem combining images: ", e);
+            }
+        }
     }
 }
